1335e63d60c04689cbfd20bd982b3baaf751f0e6
Paulo Bettencourt <pauloblemos@gmail.com>
2025-09-27 18:43:56 +0000

    view container ref

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,26 +1,33 @@
-import { Component, ComponentRef, inject, OnInit, signal, ViewContainerRef } from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  inject,
+  OnInit,
+  signal,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { Api } from '../../services/api';
 import { CommonModule, JsonPipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { EventPage } from '../event-page/event-page';
 
 @Component({
   selector: 'app-main',
-  standalone: true, // Add if this is a standalone component
+  standalone: true,
   imports: [CommonModule, JsonPipe],
   templateUrl: './main.html',
-  styleUrls: ['./main.scss'], // fixed typo
+  styleUrls: ['./main.scss'],
 })
 export class Main implements OnInit {
   apiService = inject(Api);
   router = inject(Router);
   events = signal<any>([]);
-  private viewContainer = inject(ViewContainerRef);
   componentRef: ComponentRef<EventPage> | null = null;
-  isPageEvent = this.componentRef?.hostView.destroyed;
+  isListOfEvents = signal(true);
 
-  // Keep reference to dynamically created component
+  @ViewChild('dynamicContainer', { read: ViewContainerRef, static: true })
+  viewContainer!: ViewContainerRef;
 
   ngOnInit(): void {
     this.apiService.getEvents().subscribe({
@@ -31,29 +38,29 @@ export class Main implements OnInit {
 
   goToEventDetail(eventId: number) {
     // Destroy previous component if exists
-    if (this.componentRef && !this.componentRef.hostView.destroyed) {
-      console.log('Component is not yet destroyed');
-
-      this.componentRef.destroy(); // destroy the component
-      console.log('Component destroyed:', this.componentRef.hostView.destroyed); // true
-
-      this.componentRef = null; // remove reference
-      return;
+    if (this.componentRef) {
+      this.destroyEventPage();
     }
 
-    // Create the component dynamically
+    // Hide the list and show the dynamic component
+    this.isListOfEvents.set(false);
+
+    // Create the component dynamically inside the placeholder
     this.componentRef = this.viewContainer.createComponent(EventPage);
 
-    console.log('Component has been created!', this.componentRef.instance);
+    // Pass the event data
+    this.componentRef.instance.eventDetails = eventId;
 
-    // // Pass data to component
-    // this.componentRef.instance.eventDetails = eventId;
+    console.log('Component has been created!', this.componentRef.instance);
   }
 
   destroyEventPage() {
     if (this.componentRef) {
       this.componentRef.destroy();
       this.componentRef = null;
+
+      // Show the list again
+      this.isListOfEvents.set(true);
       console.log('Component destroyed!');
     }
   }
